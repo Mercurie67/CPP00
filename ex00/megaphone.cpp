b09b42b6e9b3a9be4ac48aad7356be72a10836a7
Meru <meru334@gmail.com>
2026-03-19 17:40:50 +0100

    megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+int main(int ac, char *av[])
+{
+	int i = 1;
+	if (ac < 2)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while(av[i])
+		{
+			int j = 0;
+			while(av[i][j])
+			{
+				cout << (char)toupper(av[i][j]);
+				j++;
+			}
+			i++;
+			if (i < ac - 1)
+				cout << " ";
+		}
+	}
+	cout << "\n";
+	return 0;
+}
